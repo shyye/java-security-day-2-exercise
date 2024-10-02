5bb9a59b89f1e0529adef55ed6ffc3ee2ead9703
shyye <emma.art.dev@gmail.com>
2024-10-02 20:35:12 +0200

    Delete Book classes

--- src/main/java/com/booleanuk/api/payload/responses/BookListResponse.java
+++
@@ -1,8 +0,0 @@
-package com.booleanuk.api.payload.responses;
-
-import com.booleanuk.api.models.Book;
-
-import java.util.List;
-
-public class BookListResponse extends Response<List<Book>> {
-}
--- src/main/java/com/booleanuk/api/repositories/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.booleanuk.api.repositories;
-
-import com.booleanuk.api.models.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Integer> {
-}
